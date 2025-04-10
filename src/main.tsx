6be8de399ac10c0d7c32cec11368e5d885ad7361
Brian Le <minh100@gmail.com>
2025-04-10 00:19:22 -0700

    git rid of basename in hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename="/usPresenceTest">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
